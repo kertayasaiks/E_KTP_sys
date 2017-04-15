f564a28807bccae47a1822d241b229f7751c5b28
kertayasaiks <kertayasaiks@gmail.com>
2017-04-16 02:25:01 +0700

    Update E_KTP.java
    sal gua bingung mau isi apa di bagian yang ada hello wordnya, itu juga yang di bagian print2 itu gak tau bener atau engga, nanti gua perbaiki ya kalo ada salah, kasi tau gua yang bener gimana, suksma pak bos

--- E_KTP.java
+++ E_KTP.java
@@ -10,5 +10,42 @@ package e_ktp_system;
  * @author 0xiRiz
  */
 public class E_KTP {
+    private String ID_KTP;
+    private Akta_kelahiran Sertifikat_Akta;
+    private Asuransi Kartu_Asuransi;
+    private Riwayat_penyakit catatan_penyakit;
+    private SIM Kartu_SIM;
+    
+    public void setID_KTP(String ID){
+        this.ID_KTP = ID_KTP;
+    }
+    
+    public String getID_KTP(){
+        return ID_KTP;
+    }
+    
+    public void printAkta(){
+        //hello word!!!
+    }
+    
+    public void printRiwayatPenyakit(){
+        //hello word!!!
+    }
+    
+    public void prinAsuransi(){
+        //helllo word!!!
+    }
+    
+    public void printSIM(){
+        //hello word!!!
+    }
+    
+    public String addRiwayatPenyakit(String Nama_Penyakit, String tgl){
+        //hello word!!!
+    }
+    
+    public String addAsuransi(String No_Kartu, String tgl_awal,String tgl_akhir, String status){
+        //hello word!!
+    } 
     
 }
