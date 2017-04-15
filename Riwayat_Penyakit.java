7355beeae5d3376579da55046880aab6eb34718f
kertayasaiks <kertayasaiks@gmail.com>
2017-04-16 02:01:48 +0700

    Update Riwayat_Penyakit.java

--- Riwayat_Penyakit.java
+++ Riwayat_Penyakit.java
@@ -10,5 +10,22 @@ package e_ktp_system;
  * @author 0xiRiz
  */
 public class Riwayat_Penyakit {
+    private  String Nama_penyakit;
+    private String tgl;
     
+    public void setNama_Penyakit(String Nama_Penyakit){
+        this.Nama_Penyakit = Nama_Penyakit;
+    }
+    
+    public String getNamaPenyakit(){
+        return Nama_Penyakit;
+    }
+    
+    public void SetTanggal(String Tanggal){
+        this.Tanggal = Tanggal;
+    }
+    
+    public getTanggal(){
+       return Tanggal;
+    }
 }
